f90df3eaddbc2ef31b478cb6e117b90dd4d83cd3
Abdalla Mohamed <abdulla.mohamed.university@gmail.com>
2021-10-10 15:36:50 -0400

    Timer in the UI

--- 2021 FALL GAME JAM/Assets/Scripts/TimerScript.cs
+++ 2021 FALL GAME JAM/Assets/Scripts/TimerScript.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float timer = 240;
+    public Text timeText;
+    public bool timerRunning = false;
 
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerRunning = true;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (timerRunning)
+        {
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+            }
+            else
+            {
+                timer = 0;
+                Debug.Log("Time has run out!");
+                //End round
+            }
+        }
+        TimeDisplay(timer);
     }
+
+
+    void TimeDisplay(float DisplayTime)
+    {
+        if (DisplayTime < 0)
+        {
+            DisplayTime = 0;
+        }
+        else if (DisplayTime > 0)
+        {
+            DisplayTime += 1;
+        }
+
+        float minutes = Mathf.FloorToInt(DisplayTime / 60);
+        float seconds = Mathf.FloorToInt(DisplayTime % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
 }
